9aaad4f8715174726a199e6e451b2014ab4a339e
Jeremy Xu <jeremy@beapartof.com>
2017-12-18 17:56:50 -0500

    update for MaxCounters

--- Codility/src/MaxCounters.py
+++ Codility/src/MaxCounters.py
@@ -1,9 +1,13 @@
-
+# a = [ 9, 11, 11, 11, 9, 5, 5, 99, 19, 19, 20, 20, 20, 20, 11]
+# print(a[:])
 def solution(N, A):
     counter = [0] * N
     for index in range(len(A)):
         if 1 <= A[index] <= N:
             counter[A[index] - 1] += 1
-        if A[index] == N + 1:
+        else:
             counter = [max(counter)] * N
     return counter
+
+# Performance imporovement:
+# record the max value and only update the one hasn't been touched
