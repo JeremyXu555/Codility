845e44618ed40d39d66a71ce59ea049c2be4691b
Jeremy <jeremyxu89@gmail.com>
2018-02-26 17:25:09 -0500

    error fix

--- UdemyCourse/array.py
+++ UdemyCourse/array.py
@@ -17,9 +17,9 @@ def sum_pair(arr, k):
     # Way of thinking: the first time won't happen anything, only the target truns
     # as the num in the loop which appears at the second time is when the pair been
     #found
-        if target is not in seen:
+        if target not in seen:
             seen.add(num)
         else:
-            output.add(min(num, target), max(num, target))
+            output.add((min(num, target), max(num, target)))
 
     return output
