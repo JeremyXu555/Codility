061d747a777cbb52d32799d2c325a4382a5d4199
Jeremy Xu <jeremyxu89@gmail.com>
2018-02-27 21:23:42 -0500

    a new way for reverse string

--- UdemyCourse/recursion.py
+++ UdemyCourse/recursion.py
@@ -7,3 +7,11 @@ def reverse(s):
         m = int(len(s) / 2)
         ## swap the string left half and right half
         return reverse(s[m:]) + (reverse((s[:m])))
+
+def reverse(s):
+    if len(s) <= 1:
+        return s
+    else:
+        # grab the first character of each recursion and put it on the back
+        # of the string
+        reverse(s[1:]) + s[0]
