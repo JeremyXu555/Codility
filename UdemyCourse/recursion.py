f72148e090c4be4955cb53420f77ebaf2f35a322
Jeremy <jeremyxu89@gmail.com>
2018-02-28 11:40:24 -0500

    Grab back the solutions

--- UdemyCourse/recursion.py
+++ UdemyCourse/recursion.py
@@ -8,6 +8,33 @@ def reverse(s):
         ## swap the string left half and right half
         return reverse(s[m:]) + (reverse((s[:m])))
 
+def reverse(s):
+    if len(s) <= 1:
+        return s
+    else:
+        # grab the first character of each recursion and put it on the back
+        # of the string
+        reverse(s[1:]) + s[0]
+### Permutation
+
+def permute(s):
+
+    out = []
+
+    if len(s) == 1:
+        out = [s]
+
+    else:
+        for i, let in enumerate(s):
+
+            for perm in permute(s[:i] + s[i+1:]):
+                ## try to see the way human manually do the permutation, hold one
+                ## letter and keep changing the rest of the letters, we are actually
+                ## doing the recursion in our brain without notice it
+                out += [let + perm]
+    return out
+
+
 ### Coin Change Problems:
     ## All the possible combinations
 
@@ -26,7 +53,7 @@ def coin_change_combination(target, coins):
                 combinations[amount] += combinations[amount-coin]
 
             amount += 1
-            
+
     return combinations[target]
 
 print(coin_change_combination(12, [1, 2, 5]))
