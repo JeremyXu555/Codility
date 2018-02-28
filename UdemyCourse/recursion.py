7e2ba5186809eb9b322ed77140272cf579c975a3
Jeremy Xu <jeremyxu89@gmail.com>
2018-02-28 08:08:44 -0500

    permutation

--- UdemyCourse/recursion.py
+++ UdemyCourse/recursion.py
@@ -15,3 +15,22 @@ def reverse(s):
         # grab the first character of each recursion and put it on the back
         # of the string
         reverse(s[1:]) + s[0]
+
+### Permutation
+
+def permute(s):
+
+    out = []
+
+    if len(s) == 1:
+        out = [s]
+
+    else:
+        for i, let in enumerate(s):
+
+            for perm in permute(s[:i] + s[i+1:]):
+                ## try to see the way human manually do the permutation, hold one
+                ## letter and keep changing the rest of the letters, we are actually
+                ## doing the recursion in our brain without notice it
+                out += [let + perm]
+    return out
