45d54e5e9c551c5d492aa3404151b07946b43cc7
Jeremy Xu <jeremyxu89@gmail.com>
2018-03-04 15:48:47 -0500

    Unlimited transaction max_stock_profit solution 1

---
+++ LeetCode/MaxProfitUnlimitedTransaction_122.py
@@ -0,0 +1,30 @@
+def unlimit_transaction(price):
+
+    valley = price[0]
+    peak = price[0]
+    i = 0
+    max_profit = 0
+
+    while i < len(price) - 1:
+
+        while i < len(price) - 1 and price[i] > price[i + 1]:
+            i += 1
+        valley = price[i]
+        # basic principle: once the price grows up, the valley loop will be ended
+        print('valley', valley)
+
+        while i < len(price) - 1 and price[i] < price[i + 1]:
+            i += 1
+        peak = price[i]
+        
+        # As the i keeps adding up, the valley and peak are holding the value
+        # of two ends in a continuous phase, which will make more sense in the next
+        # solution
+        print('peak', peak)
+
+        max_profit += (peak - valley)
+
+
+    return max_profit
+
+# https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/solution/
