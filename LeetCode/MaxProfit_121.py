eb5b8282e4f011d0f32914a52ecd35de9f09d28c
Jeremy Xu <jeremyxu89@gmail.com>
2018-03-04 11:34:23 -0500

    Second solution of max profit

--- LeetCode/MaxProfit_121.py
+++ LeetCode/MaxProfit_121.py
@@ -16,3 +16,22 @@ def maxProfit(arr):
         i += 1
 
     return max_sum
+
+def maxProfit2(price):
+    # create a list to store the lowest price so far
+    # Create another list to store the max profit so far
+
+    lowest_price = [0] * len(price)
+    max_profit = [0] * len(price)
+    i = 1
+
+    while i < len(price):
+
+        lowest_price[i] = min(lowest_price[i-1], price[i])
+
+        # compare the profit of previous wiht the value of selling the
+        # price at this moment
+        max_profit[i] = max(max_profit[i-1], price[i] - lowest_price[i])
+        i += 1
+
+    return max_profit[-1]
